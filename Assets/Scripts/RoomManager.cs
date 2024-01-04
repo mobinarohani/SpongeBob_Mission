a5b8592fd61bc7a05bbe8bcafc48a648d3a92b45
mobinarohani <mobinarohani4@gmail.com>
2024-01-04 14:15:32 +0330

    new change

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -6,10 +6,14 @@ using Photon.Pun;
 
 public class RoomManager : MonoBehaviourPunCallbacks
 {
+     public GameObject player;
+
+    [Space]
+    public Transform  spawnPoint;
     // Start is called before the first frame update
     void Start()
     {
-         Debug.Log("Connecting...");
+        Debug.Log("Connecting...");
 
         PhotonNetwork.ConnectUsingSettings();
     }
@@ -23,4 +27,23 @@ public class RoomManager : MonoBehaviourPunCallbacks
         PhotonNetwork.JoinLobby();
 
     }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+
+        PhotonNetwork.JoinOrCreateRoom("test", null , null);
+
+        Debug.Log("We're Connected and in a room now!");
+
+        Invoke("PhotonInit", 10);
+    }
+
+    void PhotonInit() {
+       GameObject _player = PhotonNetwork.Instantiate (player.name, spawnPoint.position,Quaternion.identity);
+
+    //    _player.GetComponent<PlayerSetup>().IsLocalPlayer();
+
+    }
+    
 }
